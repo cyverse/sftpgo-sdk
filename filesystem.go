860a92bd5cb9d18ab079c62b8f4c720e3a13b6ca
Illyoung Choi <iychoi@email.arizona.edu>
2022-04-25 10:41:40 -0700

    Add iRODS Storage Support
    Add a new FilesystemProvider and a Configuration for iRODS Storage.

--- filesystem.go
+++ filesystem.go
@@ -28,11 +28,12 @@ const (
 	CryptedFilesystemProvider                             // Local encrypted
 	SFTPFilesystemProvider                                // SFTP
 	HTTPFilesystemProvider                                // HTTP
+	IRODSFilesystemProvider                               // iRODS Storage
 )
 
 // IsProviderSupported returns true if the specified provider is supported.
 func IsProviderSupported(provider FilesystemProvider) bool {
-	return provider >= LocalFilesystemProvider && provider <= HTTPFilesystemProvider
+	return provider >= LocalFilesystemProvider && provider <= IRODSFilesystemProvider
 }
 
 // OSFsConfig defines the configuration for local filesystem
@@ -250,6 +251,51 @@ type HTTPFsConfig struct {
 	APIKey   kms.BaseSecret `json:"api_key,omitempty"`
 }
 
+// BaseIRODSFsConfig defines the base configuration for iRODS Storage
+type BaseIRODSFsConfig struct {
+	// Endpoint as host:port for the iRODS server to connect to.
+	// If port is not specified, 1247 is used.
+	Endpoint string `json:"endpoint,omitempty"`
+	// CollectionPath is the iRODS path to a collection to be accessed.
+	CollectionPath string `json:"collection_path,omitempty"`
+	Username       string `json:"username,omitempty"`
+	// ProxyUsername is an optional value for proxy access.
+	ProxyUsername string `json:"proxy_username,omitempty"`
+	// ResourceServer is an optional value that sets iRODS Resource Server name to connect to.
+	ResourceServer string `json:"resource,omitempty"`
+	// AuthScheme is an optional value that sets authentication scheme.
+	// Select one of ['native', 'pam'].
+	// If scheme is not specified, 'native' is used.
+	AuthScheme string `json:"auth_scheme,omitempty"`
+	// RequireClientServerNegotiation determines if server-client negotiation is required when establishing a new connection
+	RequireClientServerNegotiation bool `json:"require_cs_negotiation,omitempty"`
+	// ClientServerNegotiationPolicy sets policy to be used in the client when establishing a new connection
+	// Select one of ['CS_NEG_REFUSE','CS_NEG_REQUIRE','CS_NEG_DONT_CARE']
+	ClientServerNegotiationPolicy string `json:"cs_negotiation_policy,omitempty"`
+	// SSLCACertificatePath is an optional path for SSL CA Cert.
+	// Required to use PAM auth
+	// e.g., "/etc/ssl/certs/ca-certificates.crt".
+	SSLCACertificatePath string `json:"ssl_ca_cert_path,omitempty"`
+	// SSLKeySize is an optional encryption key size for SSL.
+	// Required to use PAM auth.
+	SSLKeySize int `json:"ssl_key_size,omitempty"`
+	// SSLAlgorithm is an optional encryption algorithm for SSL.
+	// Required to use PAM auth.
+	SSLAlgorithm string `json:"ssl_algorithm,omitempty"`
+	// SSLSaltSize is an optional encryption salt size for SSL.
+	// Required to use PAM auth.
+	SSLSaltSize int `json:"ssl_salt_size,omitempty"`
+	// SSLHashRounds is an optional encryption hash rounds for SSL.
+	// Required to use PAM auth.
+	SSLHashRounds int `json:"ssl_hash_rounds,omitempty"`
+}
+
+// IRODSFsConfig defines the configuration for iRODS Storage
+type IRODSFsConfig struct {
+	BaseIRODSFsConfig
+	Password kms.BaseSecret `json:"password,omitempty"`
+}
+
 // Filesystem defines filesystem details
 type Filesystem struct {
 	Provider     FilesystemProvider `json:"provider"`
@@ -260,4 +306,5 @@ type Filesystem struct {
 	CryptConfig  CryptFsConfig      `json:"cryptconfig,omitempty"`
 	SFTPConfig   SFTPFsConfig       `json:"sftpconfig,omitempty"`
 	HTTPConfig   HTTPFsConfig       `json:"httpconfig,omitempty"`
+	IRODSConfig  IRODSFsConfig      `json:"irodsconfig,omitempty"`
 }
